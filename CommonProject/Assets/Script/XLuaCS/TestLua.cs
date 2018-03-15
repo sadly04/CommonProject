16e27b11a39fc3ac7039c8c060f73bde113233d3
苏东阳 <sudongyang@babybus.com>
2018-03-15 16:06:03 +0800

    xlua添加测试

---
+++ CommonProject/Assets/Script/XLuaCS/TestLua.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using XLua;
+
+namespace sdy.Lua
+{
+    [Hotfix]
+    public class TestLua : MonoBehaviour
+    {
+        LuaEnv luaenv = new LuaEnv();
+
+        // Use this for initialization  
+        void Start()
+        {
+        }
+
+        // Update is called once per frame  
+        void Update()
+        {
+            if (luaenv != null)
+            {
+                luaenv.Tick();
+            }
+        }
+
+        public int Add(int a, int b)
+        {
+            return a - b;
+        }
+
+        void OnGUI()
+        {
+            if (GUI.Button(new Rect(10, 100, 300, 150), "Hotfix"))
+            {
+                luaenv.DoString("require 'hotfix'");
+
+                //luaenv.DoString(@"  
+                //    xlua.hotfix(CS.test, 'Add', function(self, a, b)  
+                //      return a + b  
+                //  end)  
+                //");  
+
+                int num = Add(2, 1);
+                print(num);
+            }
+        }
+
+        void OnDestroy()
+        {
+            luaenv.Dispose();
+        }
+    }
+
+
+}
